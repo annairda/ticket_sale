2f186ffc5ba11e15e94eb3f4fb01840355f74e94
Rafal Mossakowski <rafal.mossakowski@bitgamelabs.com>
2018-12-11 17:13:07 +0100

    Basic cleanup of TicketController

--- web/controllers/ticket_controller.ex
+++ web/controllers/ticket_controller.ex
@@ -7,11 +7,11 @@ defmodule TicketShop.TicketController do
     tickets = Repo.all(Ticket)
     render conn, "index.html", tickets: tickets
   end
-    
+
   def create(conn, %{"event_id" => event_id, "ticket" => ticket_params}) do
     event = Repo.get(Event, event_id)
     changeset = Ticket.changeset(%Ticket{event: event}, ticket_params)
-    
+
     case Repo.insert(changeset) do
       {:ok, _ticket} ->
         conn
@@ -22,11 +22,9 @@ defmodule TicketShop.TicketController do
         render(conn, "new.html", changeset: changeset)
     end
   end
-    
-    def new(conn, %{"event_id" => event_id}) do
-        new_ticket = Ticket.changeset(%Ticket{})
-        render conn, "new.html", changeset: new_ticket, event_id: event_id
-    end
-    
-    
+
+  def new(conn, %{"event_id" => event_id}) do
+    new_ticket = Ticket.changeset(%Ticket{})
+    render conn, "new.html", changeset: new_ticket, event_id: event_id
+  end
 end
